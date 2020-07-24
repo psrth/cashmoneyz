78c88d8abf1dae6d1e487a379de16d0bcb2ce534
Parth Sharma <parthsharma151@gmail.com>
2020-07-24 21:25:15 +0530

    routes and rudamentary UI

---
+++ lib/home.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+void main() => runApp(MyApp());
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+          Text(
+            "Cash Moneyz App",
+            textAlign: TextAlign.center,
+          ),
+        ]),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            RaisedButton(onPressed: () {
+              Navigator.pushNamed(context, '/spend');
+            }),
+            RaisedButton(onPressed: () {
+              Navigator.pushNamed(context, '/deposit');
+            }),
+          ],
+        ),
+      ),
+    );
+  }
+}
