30340082c9b83c2338c61ac161ba01d04b6ff680
Parth Sharma <parthsharma151@gmail.com>
2020-07-25 13:03:44 +0530

    synced with google sheets, internet permissions

--- lib/deposit.dart
+++ lib/deposit.dart
@@ -1,3 +1,5 @@
+import 'package:cashmoneyz/controller.dart';
+import 'package:cashmoneyz/model/model.dart';
 import 'package:flutter/material.dart';
 import 'main.dart';
 
@@ -7,12 +9,94 @@ class deposit extends StatefulWidget {
 }
 
 class _depositState extends State<deposit> {
+  final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  // text field controllers
+  TextEditingController depositController = TextEditingController();
+  TextEditingController typeController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  String spendController = "";
+
+// Method to show snackbar with 'message'.
+  _showSnackBar(String message) {
+    final snackBar = SnackBar(content: Text(message));
+    _scaffoldKey.currentState.showSnackBar(snackBar);
+  }
+
+  void _submitForm() {
+    if (_formKey.currentState.validate()) {
+      MoneyForm moneyForm = MoneyForm(typeController.text,
+          descriptionController.text, depositController.text, spendController);
+
+      MoneyController moneyController = MoneyController((String response) {
+        print(response);
+        if (response == MoneyController.STATUS_SUCCESS) {
+          _showSnackBar("Submitted");
+        } else {
+          _showSnackBar("Error");
+        }
+      });
+
+      _showSnackBar("Sending.");
+      moneyController.submitMoney(moneyForm);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Deposit Money'),
-      ),
-    );
+        key: _scaffoldKey,
+        appBar: AppBar(
+          title: Text('Deposit Money'),
+        ),
+        body: Form(
+          key: _formKey,
+          child: Container(
+              padding: EdgeInsets.symmetric(
+                vertical: 100,
+                horizontal: 30,
+              ),
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    controller: depositController,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Enter Deposit Amount";
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(hintText: "Deposit Amount"),
+                  ),
+                  TextFormField(
+                    controller: typeController,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Enter Type";
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(hintText: "Transaction Type"),
+                  ),
+                  TextFormField(
+                    controller: descriptionController,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Enter Description";
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(hintText: "Description"),
+                  ),
+                  RaisedButton(
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    onPressed: _submitForm,
+                    child: Text('Submit Feedback'),
+                  )
+                ],
+              )),
+        ));
   }
 }
--- lib/spend.dart
+++ lib/spend.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'controller.dart';
 import 'main.dart';
+import 'model/model.dart';
 
 class spend extends StatefulWidget {
   @override
@@ -7,12 +9,94 @@ class spend extends StatefulWidget {
 }
 
 class _spendState extends State<spend> {
+  final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  // text field controllers
+  TextEditingController spendController = TextEditingController();
+  TextEditingController typeController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  String depositController = "";
+
+// Method to show snackbar with 'message'.
+  _showSnackBar(String message) {
+    final snackBar = SnackBar(content: Text(message));
+    _scaffoldKey.currentState.showSnackBar(snackBar);
+  }
+
+  void _submitForm() {
+    if (_formKey.currentState.validate()) {
+      MoneyForm moneyForm = MoneyForm(typeController.text,
+          descriptionController.text, depositController, spendController.text);
+
+      MoneyController moneyController = MoneyController((String response) {
+        print(response);
+        if (response == MoneyController.STATUS_SUCCESS) {
+          _showSnackBar("Submitted");
+        } else {
+          _showSnackBar("Error");
+        }
+      });
+
+      _showSnackBar("Sending.");
+      moneyController.submitMoney(moneyForm);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Spend Money'),
-      ),
-    );
+        key: _scaffoldKey,
+        appBar: AppBar(
+          title: Text('Deposit Money'),
+        ),
+        body: Form(
+          key: _formKey,
+          child: Container(
+              padding: EdgeInsets.symmetric(
+                vertical: 100,
+                horizontal: 30,
+              ),
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    controller: spendController,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Enter Spend Amount";
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(hintText: "Spend Amount"),
+                  ),
+                  TextFormField(
+                    controller: typeController,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Enter Type";
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(hintText: "Transaction Type"),
+                  ),
+                  TextFormField(
+                    controller: descriptionController,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Enter Description";
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(hintText: "Description"),
+                  ),
+                  RaisedButton(
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    onPressed: _submitForm,
+                    child: Text('Submit Feedback'),
+                  )
+                ],
+              )),
+        ));
   }
 }
