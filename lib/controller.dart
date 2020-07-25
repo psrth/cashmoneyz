dc0a52f39c6af69d97b8dbc94d325ddd89e94ece
Parth Sharma <parthsharma151@gmail.com>
2020-07-25 11:52:25 +0530

    http dependecy, controller

--- lib/controller.dart
+++ lib/controller.dart
@@ -1,4 +1,5 @@
-import "package:cashmoneyz/model/model.dart";
+import 'dart:convert' as convert;
+import 'package:cashmoneyz/model/model.dart';
 import 'package:http/http.dart' as http;
 
 class MoneyController {
@@ -7,9 +8,11 @@ class MoneyController {
       "https://script.google.com/macros/s/AKfycbz1krJEGhJPiMDcWnavbSt92TTEXyaDfOa7bFuSLNmbq87pQto/exec";
   static const STATUS_SUCCESS = "SUCCESS";
   MoneyController(this.callback);
-  void submitMoney(MoneyController moneyController) async {
+  void submitMoney(MoneyForm moneyForm) async {
     try {
-      await http.get()
+      await http.get(URL + moneyForm.toParams()).then((response) {
+        callback(convert.jsonDecode(response.body)['status']);
+      });
     } catch (e) {
       print(e);
     }
--- lib/model/model.dart
+++ lib/model/model.dart
@@ -1,6 +1,11 @@
-class Model {
+class MoneyForm {
   String _type;
   String _description;
   String _deposit;
   String _spend;
+
+  MoneyForm(this._type, this._description, this._deposit, this._spend);
+
+  String toParams() =>
+      "?type=$_type&?description=$_description?deposit=$_deposit?spend=$_spend";
 }
